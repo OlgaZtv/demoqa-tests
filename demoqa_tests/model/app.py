196bee81101d159b1780d6a9eaff3f10d9344a65
OlgaZtv <zatulivetrova@gmail.com>
2022-07-13 18:41:03 +0300

    added Fluent PageObject, StepsObject, AppManager

---
+++ demoqa_tests/model/app.py
@@ -0,0 +1,18 @@
+from selene import have, command
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import ss
+
+from demoqa_tests.model.pages.student_registration_form import StudentRegistrationForm
+from demoqa_tests.model.controls.table import Table
+
+form = StudentRegistrationForm()
+results = Table()
+
+
+def given_student_registration_form_opened():
+    browser.open('/automation-practice-form')
+    (
+        ss('[id^=google_ads][id$=container__]').with_(timeout=10)
+        .should(have.size_greater_than_or_equal(2))
+        .perform(command.js.remove)
+    )
---
+++ demoqa_tests/model/pages/student_registration_form.py
@@ -0,0 +1,72 @@
+from selene import have, by
+from selene.core import command
+from selene.support.shared.jquery_style import s, ss
+
+from demoqa_tests.utils.path_to_file import resourse
+
+
+class StudentRegistrationForm:
+
+    def set_first_name(self, value):
+        s('#firstName').type(value)
+        return self
+
+    def set_last_name(self, value):
+        s('#lastName').type(value)
+        return self
+
+    def set_email(self, value):
+        s('#userEmail').type(value)
+        return self
+
+    def set_mobile(self, value):
+        s('#userNumber').type(value)
+        return self
+
+    def set_gender(self, value):
+        s('#genterWrapper').all('.custom-radio').element_by(have.exact_text(value)).click()
+        return self
+
+    def set_birth_date(self, year: str, month: int, day: 'str'):
+        s('#dateOfBirthInput').click()
+        s('.react-datepicker__year-select').s(f'[value="{year}"]').click()
+        s('.react-datepicker__month-select').s(f'[value="{month}"]').click()
+        s(f'.react-datepicker__day--0{day}').click()
+        return self
+
+    def select_subjects(self, *values):
+        for value in values:
+            s('#subjectsInput').set_value(value).press_enter()
+        return self
+
+    def select_hobbies(self, *values):
+        for value in values:
+            s('#hobbiesWrapper').all('.custom-checkbox').element_by(have.exact_text(value)).click()
+        return self
+
+    def upload(self, file: str):
+        s('#uploadPicture').send_keys(resourse(file))
+        return self
+
+    def set_address(self, value):
+        s('#currentAddress').type(value)
+        return self
+
+    def set_state_city(self, state: str, city: str):
+        s('#state').perform(command.js.scroll_into_view).click()
+        s(by.text(state)).click()
+        s('#city').click()
+        s(by.text(city)).click()
+        return self
+
+    def check_modal_visible(self, value: str):
+        s('#example-modal-sizes-title-lg').should(have.exact_text(value))
+        return self
+
+    @staticmethod
+    def submit():
+        s('#submit').perform(command.js.click)
+
+    @staticmethod
+    def close_dialog():
+        s('#closeLargeModal').perform(command.js.click)
